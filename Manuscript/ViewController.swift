ce454fcda825fbcca7dab394a107f6f51228f623
Tigran Ghazinyan <me@tigranes.io>
2022-05-06 12:55:37 -0700

    Initial Commit

---
+++ Manuscript/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manuscript
+//
+//  Created by Tigran Ghazinyan on 5/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
